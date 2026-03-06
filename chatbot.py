f843de465ac9d1bb1fabb1d248bd9624648bf408
thedudeowl <asualum2@gmail.com>
2026-03-06 12:45:48 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,20 @@ import streamlit as st
 import random
 import time
 
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
 st.title("Simple chat")
 
 # Initialize chat history
